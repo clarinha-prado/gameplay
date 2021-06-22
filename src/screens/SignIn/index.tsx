5f785448d9e20dc8bf08dc5578dc78246053ee56
Clarinha Prado <clarinha.prado@gmail.com>
2021-06-22 13:03:43 -0300

    Home screen and navigation

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -2,23 +2,24 @@ import React from 'react';
 import {
     View,
     Text,
-    Image,
-    StatusBar
+    Image
 } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
 import IllustrationImg from '../../assets/illustration.png';
-import { ButtonIcon } from '../../components/ButtonIcon';
 import { styles } from './styles';
 
+import { ButtonIcon } from '../../components/ButtonIcon';
+
 export function SignIn() {
+    const navigation = useNavigation();
+
+    function handleSignIn() {
+        navigation.navigate('Home');
+    }
 
     return (
         <View style={styles.container}>
-            <StatusBar
-                barStyle='light-content'
-                backgroundColor='transparent'
-                translucent
-            />
-
             <Image
                 source={IllustrationImg}
                 style={styles.image}
@@ -27,20 +28,21 @@ export function SignIn() {
 
             <View style={styles.content}>
                 <Text style={styles.title}>
-                    Organize {`\n`}
-                    suas jogatinas  {`\n`}
-                    r facilmente
+                    Conecte-se {'\n'}
+                    e organize suas {'\n'}
+                    jogatinas
                 </Text>
 
                 <Text style={styles.subtitle}>
-                    Crie grupos para jogar seus games {`\n`}
+                    Crie grupos para jogar seus games {'\n'}
                     favoritos com seus amigos
                 </Text>
 
-                <ButtonIcon title="Entrar com Discord" activeOpacity={0.7} />
-
+                <ButtonIcon
+                    title="Entrar com Discord"
+                    onPress={handleSignIn}
+                />
             </View>
-
         </View>
-    )
+    );
 }
